bb3418c4981141ecec5a7311cd7da529a0597a8c
Daniel Morrison <dmorrison@arsproducts.us>
2019-03-14 14:38:25 -0400

    Proof of concept

---
+++ Keysight VISA IDN Test/IDN Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using Ivi.Visa.Interop;
+
+namespace Keysight_VISA_IDN_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var rm = new ResourceManager();
+            var conn = new FormattedIO488();
+            conn.IO = (IMessage) rm.Open(textboxAddress.Text);
+            conn.IO.Clear();
+            conn.WriteString("*IDN?", true);
+            MessageBox.Show(conn.ReadString());
+        }
+    }
+}
